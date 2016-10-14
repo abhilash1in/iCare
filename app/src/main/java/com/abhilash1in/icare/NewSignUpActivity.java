41757a3463f8403815f9a132d2b10a629b6ae1ec
Anisha <anisham197@gmail.com>
2016-10-14 23:56:12 +0530

    sign in, login, nav drawer done

---
+++ app/src/main/java/com/abhilash1in/icare/NewSignUpActivity.java
@@ -0,0 +1,123 @@
+package com.abhilash1in.icare;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+public class NewSignUpActivity extends AppCompatActivity implements
+        View.OnClickListener {
+
+    private static final String TAG = "EmailPassword";
+
+    private EditText mEmailField;
+    private EditText mPasswordField;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private DatabaseReference mDatabase;
+    private FirebaseUser user;
+
+    ProgressDialog progressDialog;
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_sign_up);
+        progressDialog = new ProgressDialog(NewSignUpActivity.this);
+
+        // Views
+        mEmailField = (EditText) findViewById(R.id.field_email);
+        mPasswordField = (EditText) findViewById(R.id.field_password);
+
+        // Buttons
+        findViewById(R.id.email_create_account_button).setOnClickListener(this);
+
+      /*  mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+            }
+        };*/
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        //mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+       /* if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }*/
+    }
+
+
+
+    // Checking if username and password are entered
+   /* private boolean validateForm() {
+        boolean valid = true;
+
+        String email = mEmailField.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            mEmailField.setError("Required.");
+            valid = false;
+        } else {
+            mEmailField.setError(null);
+        }
+
+        String password = mPasswordField.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            mPasswordField.setError("Required.");
+            valid = false;
+        } else {
+            mPasswordField.setError(null);
+        }
+
+        return valid;
+    }*/
+
+    // On clicking sign up button, the account is created (Added to database)
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent();
+        intent.setClass(NewSignUpActivity.this, MainActivity.class);
+        startActivity(intent);
+        //createAccount(mEmailField.getText().toString(), mPasswordField.getText().toString());
+    }
+
+
+}
